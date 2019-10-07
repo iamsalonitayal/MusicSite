b0000da01542ad2450fb6ecc2576afa92d9dbdde
Saloni Tayal <42753566+salonitayal@users.noreply.github.com>
2019-10-07 23:17:02 +0530

    Add files via upload

--- website/music/urls.py
+++ website/music/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     #/music/123 or album id/(lecture 12 tnb for this r.e.) #for url pattern
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    '''#/music/123 or album id/favorite /(lecture 22)
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite "),'''
+    #/music/123 or album id/favorite /(lecture 22)
+    #url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite "),
 ]
